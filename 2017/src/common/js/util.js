4b29e3e27889f06e20d78944bba82effc5891f41
kangsunlei <kangsunlei@qq.com>
2017-03-10 00:17:51 +0800

    imooc-vue高仿饿了么app

---
+++ 2017/src/common/js/util.js
@@ -0,0 +1,21 @@
+/**
+ * 解析url参数
+ * @example ?id=123&name=a
+ * @return Object{id:123,name:a}
+ */
+export function urlParse() {
+  let url = window.location.search;
+  let obj = {};
+  let reg = /[?&][^?&]+=[^?&]+/g;
+  let arr = url.match(reg);
+  // ['?id=123','name=a']
+  if (arr) {
+    arr.forEach((item) => {
+      let tempArr = item.substr(1).split('=');
+      let key = decodeURIComponent(tempArr[0]);
+      let val = decodeURIComponent(tempArr[1]);
+      obj[key] = val;
+    });
+  }
+  return obj;
+};
